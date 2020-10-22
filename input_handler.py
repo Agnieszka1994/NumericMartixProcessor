1c7d52f101edde5170c0d3005402e5849b042da0
Agnieszka Lasota <agnieszkalasota1994@gmail.com>
2020-10-22 12:56:32 +0200

    :hammer: structure improved

--- input_handler.py
+++ input_handler.py
@@ -1,74 +1,27 @@
-from string import digits
-
-
-def split_and_cast_to_list(digits_only_string):
-    return list(map(int, digits_only_string.split()))
-
-
-def split_and_cast_to_floats(digits_only_string):
-    return list(map(float, digits_only_string.split()))
-
-
-def digits_only_input(string_input) -> bool:
-    accepted = digits + ' -.'
-    for element in string_input:
-        if element not in accepted:
-            return False
-        return True
-    return False
-
-
-def length_correct(list_of_ints, target_length):
-    if len(list_of_ints) > target_length:
-        print('Too many arguments!')
-        return False
-    elif len(list_of_ints) < target_length:
-        print('Too few arguments!')
-        return False
-    else:
-        return True
-
-
-def input_validator(string_input, required_length):
-    string_copy = string_input
-    while not digits_only_input(string_copy):
-        string_copy = input('Invalid input! Only digits accepted! \
-        \n Enter new dimensions or "esc" to escape: ')
-        if string_copy == 'esc':
-            return [0, 0]
-
-    if digits_only_input(string_copy):
-        casted_list = split_and_cast_to_list(string_copy)
-        if length_correct(casted_list, required_length):
-            return casted_list
+def process_input(command):
+    while True:
+        try:
+            rows, columns = list(map(int, input(command).split()))
+        except ValueError:
+            print('Invalid input! You should enter exactly 2 integers!\n')
         else:
-            print('Default size [1, 1]:')
-            return [1, 1]
-    else:
-        return [1, 1]
-
-
-def process_input(user_input):
-    rows, columns = input_validator(user_input, 2)
-    matrix = validate_matrix_of_floats(rows, columns)
-    return matrix
+            return validate_matrix_of_floats(rows, columns)
 
 
 def validate_matrix_of_floats(rows, columns):
-    print('Enter matrix:')
-    matrix = []
+    print('Enter matrix: ')
+    new_matrix = []
     assigned_rows = 0
     while rows > assigned_rows:
-        user_input = input()
-        if user_input == 'esc':
-            return [[0.0]]
-        if not digits_only_input(user_input):
-            continue
+        try:
+            list_to_append = list(map(float, input().split()))
+        except ValueError:
+            print('Invalid input! You should enter only numbers!')
         else:
-            list_to_append = split_and_cast_to_floats(user_input)
-            if not length_correct(list_to_append, columns):
+            if len(list_to_append) > columns or len(list_to_append) < columns:
+                print('Incorrect number of arguments in row!')
                 continue
             else:
-                matrix.append(list_to_append)
+                new_matrix.append(list_to_append)
                 assigned_rows += 1
-    return matrix
+    return new_matrix
--- main.py
+++ main.py
@@ -8,10 +8,8 @@ SINGLE_INPUT = 'Enter size of matrix: '
 CONST_INPUT = 'Enter constant: '
 
 
-while True:
-    
-    
-    
+while True:  
+    os.system('cls')
     print('1. Add matrices')
     print('2. Multiply matrix by a constant')
     print('3. Multiply matrices')
@@ -19,75 +17,75 @@ while True:
     print('5. Calculate a determinant')
     print('6. Inverse matrix')
     print('0. Exit')
-    choice = input('Your choice: ')
-    if choice not in digits:
-        print('Only digits from 0 to 6 allowed!')
-        input("Press Enter to continue...")
+    try:
+        choice = int(input('Your choice: '))
+    except ValueError:
+        print('Only digits from 0 to 5 allowed!')
+        input('Press enter to continue...')
         continue
 
-    choice = int(choice)
-    # ------------------------------------------------------------------------------------------------
-    if choice == 1:
+    else:
+        # ------------------------------------------------------------------------------------------------
+        if choice == 1:
 
-        matrix_a = Matrix(process_input(input(FIRST_INPUT)))
-        matrix_b = Matrix(process_input(input(SECOND_INPUT)))
-        matrix_a.add_matrix(matrix_b)
-    # ------------------------------------------------------------------------------------------------
-    elif choice == 2:
+            matrix_a = Matrix(process_input(FIRST_INPUT))
+            matrix_b = Matrix(process_input(SECOND_INPUT))
+            matrix_a.add_matrix(matrix_b)
+        # ------------------------------------------------------------------------------------------------
+        elif choice == 2:
 
-        matrix_a = Matrix(process_input(input(SINGLE_INPUT)))
-        matrix_a.multiply_by_scalar(float(input(CONST_INPUT)))
-    # ------------------------------------------------------------------------------------------------
-    elif choice == 3:
+            matrix_a = Matrix(process_input(SINGLE_INPUT))
+            matrix_a.multiply_by_scalar(float(CONST_INPUT))
+        # ------------------------------------------------------------------------------------------------
+        elif choice == 3:
 
-        matrix_a = Matrix(process_input(input(FIRST_INPUT)))
-        matrix_b = Matrix(process_input(input(SECOND_INPUT)))
-        matrix_a.multiply_by_matrix(matrix_b)
-    # ------------------------------------------------------------------------------------------------
-    elif choice == 4:
-        print('1. Main diagonal')
-        print('2. Side diagonal')
-        print('3. Vertical line')
-        print('4. Horizontal line')
-        option = int(input('Your choice: '))
+            matrix_a = Matrix(process_input(FIRST_INPUT))
+            matrix_b = Matrix(process_input(SECOND_INPUT))
+            matrix_a.multiply_by_matrix(matrix_b)
+        # ------------------------------------------------------------------------------------------------
+        elif choice == 4:
+            print('1. Main diagonal')
+            print('2. Side diagonal')
+            print('3. Vertical line')
+            print('4. Horizontal line')
+            option = int(input('Your choice: '))
+            # --------------------------------------------------------------------------------------------
+            if option == 1:
+
+                matrix = Matrix(process_input(SINGLE_INPUT))
+                matrix.transpose_main()
+            # --------------------------------------------------------------------------------------------
+            elif option == 2:
+
+                matrix = Matrix(process_input(SINGLE_INPUT))
+                matrix.transpose_side()
+            # --------------------------------------------------------------------------------------------
+            elif option == 3:
+
+                matrix = Matrix(process_input(SINGLE_INPUT))
+                matrix.transpose_vertical()
+            # --------------------------------------------------------------------------------------------
+            elif option == 4:
+
+                matrix = Matrix(process_input(SINGLE_INPUT))
+                matrix.transpose_horizontal()
+            # --------------------------------------------------------------------------------------------
+            else:
+                print('No such option available!')
+                break
+        # ------------------------------------------------------------------------------------------------
+        elif choice == 5:
+
+            matrix = Matrix(process_input(SINGLE_INPUT))
+            print(f'The result is: \n{matrix.determinantOfMatrix(matrix.matrix)}')
+        # ------------------------------------------------------------------------------------------------
+        elif choice == 6:
+
+            matrix = Matrix(process_input(SINGLE_INPUT))
+            matrix.inverse()
         # --------------------------------------------------------------------------------------------
-        if option == 1:
-
-            matrix = Matrix(process_input(input(SINGLE_INPUT)))
-            matrix.transpose_main()
-        # --------------------------------------------------------------------------------------------
-        elif option == 2:
-
-            matrix = Matrix(process_input(input(SINGLE_INPUT)))
-            matrix.transpose_side()
-        # --------------------------------------------------------------------------------------------
-        elif option == 3:
-
-            matrix = Matrix(process_input(input(SINGLE_INPUT)))
-            matrix.transpose_vertical()
-        # --------------------------------------------------------------------------------------------
-        elif option == 4:
-
-            matrix = Matrix(process_input(input(SINGLE_INPUT)))
-            matrix.transpose_horizontal()
-        # --------------------------------------------------------------------------------------------
+        elif choice == 0:
+            break
         else:
             print('No such option available!')
-            break
-    # ------------------------------------------------------------------------------------------------
-    elif choice == 5:
-
-        matrix = Matrix(process_input(input(SINGLE_INPUT)))
-        print(f'The result is: \n{matrix.determinantOfMatrix(matrix.matrix)}')
-    # ------------------------------------------------------------------------------------------------
-    elif choice == 6:
-
-        matrix = Matrix(process_input(input(SINGLE_INPUT)))
-        matrix.inverse()
-    # --------------------------------------------------------------------------------------------
-    elif choice == 0:
-        break
-    else:
-        print('No such option available!')
-
-    input("Press Enter to continue...")
+        input('Press enter to continue...')
